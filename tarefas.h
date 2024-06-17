2d6e63b47bd298f08239d109c2012459f9f710ed
caio altmuller <150199353+Catitomio11@users.noreply.github.com>
2024-06-16 21:04:40 -0300

    Update tarefas.h

--- tarefas.h
+++ tarefas.h
@@ -1,23 +1,24 @@
 #ifndef TAREFAS_H
 #define TAREFAS_H
 
-#define TAM_CATEGORIA 50
-#define TAM_DESCRICAO 100
+#include <stdio.h>
+
+#define MAX_CATEGORIA 50
+#define MAX_DESCRICAO 255
 #define MAX_TAREFAS 100
 
 typedef struct {
-    char categoria[TAM_CATEGORIA];
-    char descricao[TAM_DESCRICAO];
+    char categoria[MAX_CATEGORIA];
+    char descricao[MAX_DESCRICAO];
     int prioridade;
 } Tarefa;
 
-void remover_quebra_linha(char *str);
-void criar_tarefa(Tarefa *tarefa, const char *categoria, const char *descricao, int prioridade);
-void listar_tarefas(Tarefa *tarefas, int num_tarefas, const char *categoria);
-void exportar_tarefas_txt(Tarefa *tarefas, int num_tarefas, const char *categoria);
-void deletar_tarefa(Tarefa *tarefas, int *num_tarefas, const char *categoria, const char *descricao);
-int carregar_tarefas_bin(Tarefa *tarefas, const char *nome_arquivo);
-void salvar_tarefas_bin(Tarefa *tarefas, int num_tarefas, const char *nome_arquivo);
-void atualizar_arquivo_txt(Tarefa *tarefas, int num_tarefas);
+void trim_newline(char* str);
+void adicionar_tarefa(Tarefa tarefas[], int* num_tarefas);
+void listar_tarefas(Tarefa tarefas[], int num_tarefas);
+void exportar_tarefas(Tarefa tarefas[], int num_tarefas);
+void carregar_tarefas(Tarefa tarefas[], int* num_tarefas);
+void salvar_tarefas(Tarefa tarefas[], int num_tarefas);
+void deletar_tarefa(Tarefa tarefas[], int* num_tarefas);
 
 #endif
